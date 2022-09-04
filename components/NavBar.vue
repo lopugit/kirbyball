3fa2209455fde390508d05d51bcfcc39b7a72b97
Nikolaj Frey <lopu@lopusmbp13.local>
2022-09-04 15:01:00 +1000

    master

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="w-full flex items-center justify-center bg-primary py-2 shadow-sm">
-    <div class="container px-2">
+    <div class="container px-2 flex flex-row items-center">
+      <img src="icon.png" style="height: 25px" class="pr-2">
       <div
         class="text-secondary font-bold text-2xl"
       >
